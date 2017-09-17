10b93dafb24f9ec34cc0c7aba2e8a3bfec4f3460
shanksluffy <wtopmone@gmail.com>
2017-09-17 21:01:06 +0800

    add spider sample part two

--- baike_spider/baike_spider/html_outputer.py
+++ baike_spider/baike_spider/html_outputer.py
@@ -6,3 +6,39 @@ Created on 2017-7-23
 
     
     
+class HtmlOutputer(object):
+    
+    def __init__(self):
+        self.datas = []
+    
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
+
+    
+    def output_html(self):
+        fout = open('output.html', 'w')
+        fout.write("<html>")
+        fout.write("<body>")
+        fout.write("<table>")
+        
+        #default mode is ascii
+        for data in self.datas:
+            fout.write("<tr>")
+            fout.write("<td>%s</td>" % data['url'])
+            fout.write("<td>%s</td>" % data['title'].encode('utf-8'))
+            fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
+            fout.write("</tr>")
+            
+        fout.write("</table>")
+        fout.write("</body>")
+        fout.write("</html>")
+        fout.close()
+    
+    
+    
+    
+
+
+
--- baike_spider/baike_spider/html_parser.py
+++ baike_spider/baike_spider/html_parser.py
@@ -5,16 +5,37 @@ Created on 2017-7-23
 '''
 #todo add lib bs4
 from bs4 import BeautifulSoup
+import re
+import urlparse
 
 class HtmlParser(object):
     
     
     def _get_new_urls(self, page_url, soup):
-        pass
+        new_urls = set()
+        #/item/%E8%A7%A3%E9%87%8A%E5%99%A8
+        links = soup.find_all('a', href=re.compile(r'/item/*'))
+        for link in links:
+            new_url = link['href']
+            new_full_url = urlparse.urljoin(page_url, new_url)
+            new_urls.add(new_full_url)
+        return new_urls
     
     
     def _get_new_data(self, page_url, soup):
-        pass
+        res_data = {}
+        #url
+        res_data['url'] = page_url
+        
+        #<dd class="lemmaWgt-lemmaTitle-title"> <h1>Python</h1>
+        title_node = soup.find('dd', class_ = "lemmaWgt-lemmaTitle-title").find("h1")
+        res_data['title'] = title_node.get_text()
+        
+        #<div class="lemma-summary" label-module="lemmaSummary">
+        summary_node = soup.find('div', class_ = "lemma-summary")
+        res_data['summary'] = summary_node.get_text()
+        
+        return res_data
     
     
     def parse(self, page_url, html_cont):
